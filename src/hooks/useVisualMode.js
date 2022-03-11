3383bcdcd6d2f7cfbf87dcfe1df86ae4ffb450b4
Triple the Boba ! <coconuttt1010@gmail.com>
2022-03-11 00:39:30 -0800

    build custom Hooks to handle interactions for appointment create/edit, save/delete, confirm

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,21 @@
+import React, {useState} from "react";
+
+export default function useVisualMode(initial) {
+  
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(mode, replace = false) {
+    setMode(mode);
+    if (replace) setHistory(history => [...history.slice(0, -1), mode]);
+    else setHistory(history => [...history, mode]);
+  };
+
+  function back() {
+    if (history.length === 1) return;
+    setHistory(history.slice(0, -1));
+    setMode(history[history.length - 2]);
+  };
+
+  return {mode, transition, back};
+}
