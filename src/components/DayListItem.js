2536a15ad4addbb2f975c852ddbbb65d0013fd4b
Triple the Boba ! <coconuttt1010@gmail.com>
2022-03-11 06:48:10 -0800

    add cypres test case for Appointment

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -32,6 +32,8 @@ export default function DayListItem(props) {
       onClick={() => {props.setDay(props.name)}}
       selected={props.selected}
       className={dayClass}
+      
+      data-testid="day"
     >
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
